85954b75ad8d2a95f99915f2d80861c892e9c592
abdulwahabone <abdulwahabone@gmail.com>
2022-07-08 03:33:27 +0800

    Add meta details

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# fresh project
+# Playing with Deno Fresh
 
-### Usage
+<img align="right" src="https://raw.githubusercontent.com/denoland/fresh/main/www/static/logo.svg" height="150px" alt="the fresh logo: a sliced lemon dripping with juice">
+
+[**Demo**](https://abdulwahabone-deno-fresh.deno.dev/)
+#### This demo includes
+- Static home page
+- Page with a counter, using islands (only page with js shipped)
+- SSR random jokes
+- SSR random memes
 
 Start the project:
-
 ```
 deno task start
 ```
-
 This will watch the project directory and restart as necessary.
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,12 +1,36 @@
 /** @jsx h */
-import { h } from "preact";
+import { Fragment, h } from "preact";
+import { Head , asset} from "$fresh/runtime.ts";
+
 import HomeHeader from "../components/home/header/homeHeader.tsx";
 import Project from "../components/home/projects/project.tsx";
 import Footer from "../components/home/footer/footer.tsx";
 
+const title = "Playing with Deno Fresh";
+const desc = "Hi, I'm Wahab. I'm testing out Deno's new web framework, Fresh!";
+
 export default function Home() {
   return (
-    <div>
+    <Fragment>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content={desc}
+        />
+        <meta
+          property="og:title"
+          content={title}
+        />
+        <meta
+          property="og:description"
+          content={desc}
+        />
+        <meta
+          property="og:image"
+          content={asset("/meta.jpg")}
+        />
+      </Head>
       <HomeHeader />
       <Project />
       <Project invert />
@@ -17,6 +41,6 @@ export default function Home() {
       <Project />
       <Project invert />
       <Footer />
-    </div>
+    </Fragment>
   );
 }
