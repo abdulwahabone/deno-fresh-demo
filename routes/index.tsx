4bfee1a294c2b701d9e8a54c0efb3a940bcb4c22
abdulwahabone <abdulwahabone@gmail.com>
2022-07-08 03:38:13 +0800

    Fix meta content

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,15 +1,16 @@
 /** @jsx h */
 import { Fragment, h } from "preact";
-import { Head , asset} from "$fresh/runtime.ts";
+import { asset, Head } from "$fresh/runtime.ts";
 
 import HomeHeader from "../components/home/header/homeHeader.tsx";
 import Project from "../components/home/projects/project.tsx";
 import Footer from "../components/home/footer/footer.tsx";
 
-const title = "Playing with Deno Fresh";
-const desc = "Hi, I'm Wahab. I'm testing out Deno's new web framework, Fresh!";
-
 export default function Home() {
+  const title = "Playing with Deno Fresh";
+  const desc =
+    "Hi, I'm Wahab. I'm testing out Deno's new web framework, Fresh!";
+
   return (
     <Fragment>
       <Head>
@@ -28,7 +29,7 @@ export default function Home() {
         />
         <meta
           property="og:image"
-          content={asset("/meta.jpg")}
+          content="https://abdulwahabone-deno-fresh.deno.dev/meta.jpg"
         />
       </Head>
       <HomeHeader />
